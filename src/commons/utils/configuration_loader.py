95e1f5cf31668999dd680a56cb5bb047a9f9eb96
TheoV <vermerschth@gmail.com>
2015-11-01 01:14:49 +0100

    updating mongo_driver label

--- src/commons/utils/configuration_loader.py
+++ src/commons/utils/configuration_loader.py
@@ -34,31 +34,31 @@ class configuration_loader(object):
 		self.mongo_properties = self.parser.get_value_by_key("mongo")
 
 		if self.mongo_properties is "":
-			raise ConfigurationException("Hestia Node network configuration isn't present")
+			raise ConfigurationException("Mongo Node is missing")
 
 		if not "mongo_host" in self.mongo_properties.keys() or not "mongo_port" in self.mongo_properties.keys() :
-			raise ConfigurationException("Hestia Node missing some keys")
+			raise ConfigurationException("Mongo Node missing some keys")
 
 		if self.mongo_properties["mongo_host"] is "" or self.mongo_properties["mongo_port"] is "" or (not int(self.mongo_properties["mongo_port"]) ):
-			raise ConfigurationException("Board's name not valid")
+			raise ConfigurationException("Mongo configuration is not valid")
 
 	def set_board_properties(self):
 		self.board_properties = self.parser.get_value_by_key("board")
 
 		if self.board_properties is "":
-			raise ConfigurationException("Board's node isn't present")
+			raise ConfigurationException("Board Node is missing")
 
 		if not "name" in self.board_properties.keys():
-			raise ConfigurationException("Board's missing some keys")
+			raise ConfigurationException("Board Node missing some keys")
 
 		if self.board_properties["name"] is "":
-			raise ConfigurationException("Board's name not valid")
+			raise ConfigurationException("Board name is empty")
 
 	def set_agents_datas(self):
 		self.agents_datas = self.parser.get_value_by_key("agents")
 
 		if self.agents_datas == "":
-			raise ConfigurationException("Agents's Node is not present !")
+			raise ConfigurationException("Agents's Node is missing !")
 
 		for agent in self.agents_datas:
 			if (not "name" in agent.keys()) or (not "pin_type" in agent.keys()) or (not "pin" in agent.keys()) or (not "pin_mode" in agent.keys()):
